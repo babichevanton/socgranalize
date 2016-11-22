31bf579cb2cd4f34a9ac09ef09b11a310e80111c
Dandelion <bav.dandellion@gmail.com>
2016-11-22 05:24:47 +0300

    add neighbourhood evaluation for some node

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import os.path
+from copy import deepcopy
 
 
 def sample_extract(file):
@@ -8,6 +10,8 @@ def sample_extract(file):
     # list of elems. Elem is dict with 'id' as node index and 'value' as list of neighbours
     list_sample = map(lambda x: json.loads(x), strings)
 
+    print list_sample[0]
+
     # convert to dict of nodes with node index as key and list of neighbours as value
     sample = {}
     for node in list_sample:
@@ -16,23 +20,59 @@ def sample_extract(file):
     return sample
 
 
+def make_undirected(graph):
+    res = {}
+    for node in graph:
+        res[node] = set(graph[node])
+
+    for node in graph:
+        for neighbour in graph[node]:
+            res[neighbour].add(node)
+
+    for node in res:
+        res[node] = sorted(list(res[node]))
+
+    return res
+
+
 def neighbour_nodes(graph, node, dist):
     nodes = set([node])
-    neighbours = set([])
+    seen = set([])
+    neighbours = set([node])
     for i in xrange(dist):
         for node in nodes:
-            neighbours = neighbours.union(set(graph[node]))
-        nodes = neighbours
-        neighbours = set([])
+            seen.add(node)
+            neighbours.update(set(graph[node]))
+        nodes = neighbours.difference(seen)
 
-    return list(nodes)
+    return list(neighbours)
 
+
+def neighbourhood(graph, nodes):
+    subgraph = {}
+
+    for node in nodes:
+        subgraph[node] = filter(lambda x: x in nodes, graph[node])
+
+    return subgraph
 
 
 def main():
-    samplefile = 'data/graphSample.json'
+    # samplefile = 'data/graphSample.json'
+    samplefile = 'data/tmpsample.json'
 
     sample = sample_extract(samplefile)
+
+    undir_sample = make_undirected(sample)
+
+    nodes = sorted(neighbour_nodes(undir_sample, 1, 2))
+
+    print nodes
+
+    subgraph = neighbourhood(sample, nodes)
+
+    for node in subgraph:
+        print node, subgraph[node]
 
     return 0
 
